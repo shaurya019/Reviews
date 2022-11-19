a2438edca2a084a5d9a43e88ce016895f2b804ad
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2022-11-19 20:14:17 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
-
+import Review from './Component/review'
 function App() {
   return (
-    <div className="App">
-      <h1>Reviews</h1>
-      <reviews />
-    </div>
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
   );
 }
 
---
+++ src/Component/review.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import people from './data';
+
+const review = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default review
